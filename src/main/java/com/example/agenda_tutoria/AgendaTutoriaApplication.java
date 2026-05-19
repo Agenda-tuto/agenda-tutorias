0f8b914de9819dc134e1dc150021730bbde7823e
Agenda-tuto <agendatutorias@gmail.com>
2026-05-18 22:34:52 -0500

    Update AgendaTutoriaApplication.java

--- src/main/java/com/example/agenda_tutoria/AgendaTutoriaApplication.java
+++ src/main/java/com/example/agenda_tutoria/AgendaTutoriaApplication.java
@@ -2,10 +2,12 @@ package com.example.agenda_tutoria;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableAsync
 public class AgendaTutoriaApplication {
     public static void main(String[] args) {
         SpringApplication.run(AgendaTutoriaApplication.class, args);
